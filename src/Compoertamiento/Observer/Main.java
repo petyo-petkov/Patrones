2656c0e51bb9720c01543361f76268eaff91d942
petyo-petkov <115866546+petyo-petkov@users.noreply.github.com>
2022-12-19 08:24:47 +0100

    creando receptores

--- src/Compoertamiento/Observer/Main.java
+++ src/Compoertamiento/Observer/Main.java
@@ -3,5 +3,15 @@ package Compoertamiento.Observer;
 public class Main {
     public static void main(String[] args) {
 
+        Emisora emisora = new Emisora();
+
+        ReceptorTV tv = new ReceptorTV();
+        ReceptorRadio radio = new ReceptorRadio();
+
+        emisora.meteReceptor(tv);
+        emisora.meteReceptor(radio);
+
+        emisora.emite();
+
     }
 }
