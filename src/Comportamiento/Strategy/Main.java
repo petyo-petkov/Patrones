6104250d2b49a66a46796d7a745271cf7e510163
petyo-petkov <115866546+petyo-petkov@users.noreply.github.com>
2022-12-21 08:54:25 +0100

    creando envoltorio

--- src/Comportamiento/Strategy/Main.java
+++ src/Comportamiento/Strategy/Main.java
@@ -1,17 +1,25 @@
 package Comportamiento.Strategy;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        UsuariosFichero usuarios = new UsuariosFichero();
+        UsuariosMemoria usuarios = new UsuariosMemoria();
 
-        usuarios.crear("Petyo");
-        usuarios.crear("Elsa");
-        usuarios.crear("Irene");
+        crear(usuarios, "Petyo");
+        crear(usuarios, "Elsa");
+        crear(usuarios,"Irene");
 
-        for (String usuario : usuarios.listar()){
+        for (String usuario : listar(usuarios)){
             System.out.println(usuario);
         }
+    }
+    public static void crear (Usuarios usuarios, String nombre) {
+        usuarios.crear(nombre);
+    }
+    public static  ArrayList<String> listar(Usuarios usuarios){
+        return usuarios.listar();
     }
 
 }
